9bf7d7c431518e4cb1ab295e26368c38e000777a
rosieyzh <rosieyzh@gmail.com>
2021-01-16 16:46:09 -0700

    added embedded html

--- extension/src/embed.js
+++ extension/src/embed.js
@@ -3,22 +3,53 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Dock from 'react-dock';
+import icon from './mask.png';
 
 class InjectApp extends Component {
 
   render() {
     return (
       <div>
-        See customer experiences during COVID-19
+        <button>
+          <div class="ugiz4pqJLAG__content">
+            <div class="ugiz4pqJLAG__icon-container"><img class="ugiz4pqJLAG__icon" src={icon} /></div>
+            <div class="ugiz4pqJLAG__text">COVID-19 Safety Customer Reviews > </div>
+          </div>
+        </button>
       </div>
     );
   }
 }
 
-window.addEventListener('load', () => {
-  const injectDOM = document.createElement('div');
-  injectDOM.className = 'inject-react-example';
-  injectDOM.style.textAlign = 'center';
-  document.body.appendChild(injectDOM);
-  render(<InjectApp />, injectDOM);
-});
+var oldHref = document.location.href;
+window.onload = function() {
+    var bodyList = document.querySelector("body")
+        ,observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (oldHref != document.location.href) {
+                    oldHref = document.location.href;
+                    const injectDOM = document.createElement('div');
+                    injectDOM.className = 'inject-react';
+                    injectDOM.style.textAlign = 'left';
+                    injectDOM.style.padding = '0px 0 10px 0';
+                    injectDOM.style.fontSize = '14px';
+                    injectDOM.style.color = "#2B2D2F";
+                    var parent_div;
+                    parent_div = document.querySelector(".section-hero-header-title");
+                    if (parent_div) {
+                      parent_div.appendChild(injectDOM);
+                    }
+                    else {
+                      document.body.appendChild(injectDOM);
+                    }
+                    render(<InjectApp />, injectDOM);
+                }
+            });
+        });
+
+    var config = {
+        childList: true,
+        subtree: true
+    };
+    observer.observe(bodyList, config);
+};
