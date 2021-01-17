48f0965513017ba2e25a3a1ebec1c716b731ad1d
annashiheart <75702816+annashiheart@users.noreply.github.com>
2021-01-16 19:52:42 -0500

    new logo and description

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,9 +1,12 @@
 /*global chrome*/
+//replace with another photo
+//look into App.cs
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './newicon.svg';
 import './App.css';
 import Form from './form.js';
+import Info from './info.js';
 
 class App extends Component {
   render() {
@@ -11,17 +14,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           {this.props.isExt ? 
-            <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
+            <img src={chrome.runtime.getURL("static/media/newicon.svg")} className="App-logo" alt="logo" />
           :
             <img src={logo} className="App-logo" alt="logo" />
-          }
+          } 
 
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">COVID-19 Reviews</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          The COVID-19 pandemic is pretty scary. Find the stores that can provide you with the safest environment and share your own experiences with other users.
         </p>
         <Form></Form>
+        <Info></Info>
       </div>
     );
   }
